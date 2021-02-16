c86bf94fac1a3457456ea6ced5697d70ce2a1b21
Lucie <PC@10.11.8.63>
2021-02-16 19:59:15 +0100

    add recherhce + fichier yaml

--- Kale/lib/Recherche.dart
+++ Kale/lib/Recherche.dart
@@ -9,17 +9,66 @@ class Recherche extends StatefulWidget {
   }
 }
 
+
+
 class _RechercheState extends State<Recherche> {
+
+  TextEditingController _textController = TextEditingController();
+  List<String> initialList = ["Chat", "Chien", "Rat", "Cheval", "Ours"];
+  List<String> filteredList = List();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Page 2'),
       ),
-      body: Center(
-        child: Text('Ceci est la page 3'),
-      ),
+      body: Column(
+        children: <Widget>[TextField(
+  controller: _textController,
+  onChanged: (text) {
+  text = text.toLowerCase();
+  setState(() {
+  filteredList = initialList
+      .where((element) => element.toLowerCase().contains(text))
+      .toList();
+  });
+  },
+  ),
+        if (filteredList.length == 0 && _textController.text.isEmpty)
+            Expanded(
+            child: ListView.builder(
+            itemCount: initialList.length,
+            itemBuilder: (BuildContext context, index) {
+            return Container(
+            height: 50,
+            child: Text(initialList[index]),
+        );
+  }))
+        else if (filteredList.length==0 && _textController.text.isNotEmpty)
+            Expanded(
+            child: Container(
+            child: Text('Aucune donnée'),
+            ),
+        )
+      else
+          Expanded(
+          child: ListView.builder(
+          itemCount: filteredList.length,
+          itemBuilder: (BuildContext context, index) {
+          return Container(
+          height: 50,
+          child: Text(filteredList[index]),
+          );
+          }),
+          ),
+                ],
+          ),
+
       bottomNavigationBar: Menu(currentindex : 2),
     );
   }
+
+
 }
+
