6d237969e8eb32f788877fa4eb232be83c94518e
Lucie <luciole.lejeune@gmail.com>
2021-03-07 22:35:00 +0100

    ajout front

--- Kale/lib/Ecoresp.dart
+++ Kale/lib/Ecoresp.dart
@@ -183,6 +183,142 @@ class _EcorespState extends State<Ecoresp> {
                                 style : TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
                             ),
                         ),
+                            Row(
+                                children: [
+                                  Container(
+                                    width: 85.0,
+                                    height: 120.0,
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                            color: Colors.lightGreen,
+                                            style: BorderStyle.solid,
+                                            width: 3.0),
+                                        color: Colors.lightGreen[50],
+
+                                        borderRadius: BorderRadius.circular(35.0)),
+                                    child: Container(
+                                        padding: EdgeInsets.all(5.0),
+                                        child : Column(
+                                            children : [
+                                              Container(
+                                                child : Text('n°1', textAlign: TextAlign.center,
+                                                style : TextStyle(fontSize: 10.0, fontWeight: FontWeight.bold),
+                                        )
+                                        ),
+                                              SizedBox(height: 5.0),
+                                              Container(
+                                                  child : Text('intitulé mission', textAlign: TextAlign.center,
+                                                    style : TextStyle(fontSize: 10.0),
+                                                  )
+                                              ),
+                                            ]
+                                  ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 10.0),
+                                  Expanded(child: Container(
+                                    width: 105.0,
+                                    height: 120.0,
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                            color: Colors.lightGreen,
+                                            style: BorderStyle.solid,
+                                            width: 3.0),
+                                        color: Colors.lightGreen[50],
+
+                                        borderRadius: BorderRadius.circular(35.0)),
+                                    child: Container(
+                                      padding: EdgeInsets.all(5.0),
+                                      child : Column(
+                                          children : [
+                                            Container(
+                                                child : Text('n°2', textAlign: TextAlign.center,
+                                                  style : TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),
+                                                )
+                                            ),
+                                            SizedBox(height: 5.0),
+                                            Expanded(
+                                              child: Container(
+                                                  child : Text('intitulé mission', textAlign: TextAlign.center,
+                                                    style : TextStyle(fontSize: 12.0),
+                                                  )
+                                              ),
+                                            ),
+
+                                            SizedBox(height: 5.0),
+                                            Expanded(
+                                                child: Container(
+
+                                                    padding : EdgeInsets.only(left:18.0, top:21.0,right: 18.0),
+                                                    child: Row(
+                                                      children: [
+                                                        Container(
+                                                          height: 20.0 ,
+                                                          width: 20.0,
+
+                                                          child : GestureDetector(
+                                                            onTap: () {},
+                                                            child: Container(
+                                                              child: Icon(Icons.clear, color: Colors.red),
+                                                            ),
+                                                          ),
+                                                        ),
+                                                        SizedBox(width: 3.0),
+                                                        Container(
+                                                          height: 20.0 ,
+                                                          width: 20.0,
+
+                                                          child : GestureDetector(
+                                                            onTap: () {},
+                                                            child: Container(
+
+                                                              child: Icon(Icons.done, color: Colors.lightGreen),
+                                                            ),
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    )
+                                                )
+                                            )
+                                          ]
+                                      ),
+                                    ),
+                                  ),
+                                  ),
+                                  SizedBox(width: 10.0),
+                                  Expanded(
+                                      child : Container(
+                                        width: 85.0,
+                                        height: 120.0,
+                                        decoration: BoxDecoration(
+                                            border: Border.all(
+                                                color: Colors.lightGreen,
+                                                style: BorderStyle.solid,
+                                                width: 3.0),
+                                            color: Colors.lightGreen[50],
+
+                                            borderRadius: BorderRadius.circular(35.0)),
+                                        child: Container(
+                                          padding: EdgeInsets.all(5.0),
+                                          child : Column(
+                                              children : [
+                                                Container(
+                                                    child : Text('n°3', textAlign: TextAlign.center,
+                                                      style : TextStyle(fontSize: 10.0, fontWeight: FontWeight.bold),
+                                                    )
+                                                ),
+                                                SizedBox(height: 5.0),
+                                                Container(
+                                                    child : Text('intitulé mission', textAlign: TextAlign.center,
+                                                      style : TextStyle(fontSize: 10.0),
+                                                    )
+                                                )]
+                                          ),
+                                        ),
+                                      )
+                                  )
+                                ],
+                              ),
                     ],
                     ),
         ),
@@ -192,11 +328,43 @@ class _EcorespState extends State<Ecoresp> {
                     Row(
                       children: [
                         Container(
-                          child: Text('Niveau'),
+                          child : Expanded(
+                            child : Container (
+                              width: 100.0,
+                              padding: EdgeInsets.only(left : 35.0, top: 25.0, right: 10.0),
+                              child: Material(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  shadowColor: Colors.black26,
+                                  color: Colors.white,
+                                  elevation: 7.0,
+
+                                  child : Text('Niveau',
+                                    textAlign: TextAlign.left,
+                                    style : TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                            )
+                          ),
                         ),
-                        Container(
-                          child: Text('Badges'),
-                        )
+                          ),
+                        ),
+                              SizedBox(width: 10.0,),
+                              Container(child : Expanded(
+                                child : Container (
+                                  width: 100.0,
+                                  padding: EdgeInsets.only(left : 10.0, top: 25.0, right: 35.0),
+                                  child: Material(
+                                      borderRadius: BorderRadius.circular(15.0),
+                                      shadowColor: Colors.black26,
+                                      color: Colors.white,
+                                      elevation: 7.0,
+
+                                      child : Text('Badges',
+                                        textAlign: TextAlign.left,
+                                        style : TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                                      )
+                                  ),
+                                ),
+                              ),
+                              ),
                       ],
                     ),
 
